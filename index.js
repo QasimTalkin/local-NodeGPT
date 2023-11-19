891f7662de3577df638ba66847885e74e9f0d6b8
Qasim <qasim@Qasims-MacBook-Pro.local>
2023-11-19 11:43:01 -0500

    epress app to CLI gpt

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-require("dotenv").config();
-const express = require("express");
-const OpenAIApi = require("openai");
 
-const app = express();
-app.use(express.json());
-
+import dotenv from "dotenv";
+import OpenAIApi from "openai";
+import chalk from "chalk";
+import readline from "readline";
+// const app = express();
+// app.use(express.json());
+dotenv.config()
 
 console.log(process.env.OPENAI_API_KEY);
 
@@ -14,15 +15,64 @@ const openai = new OpenAIApi({
 
 const port = process.env.PORT || 1212;
 
-app.post("/ask", async (req, res) => {
-  const prompt = req.body.prompt;
+
+const logRed = (text) => console.log(chalk.red(text));
+
+const logGreen = (text) => console.log(chalk.green(text));
+
+
+
+// app.post("/ask", async (req, res) => {
+//   const prompt = req.body.prompt;
+
+//   try {
+//     if (prompt == null) {
+//       throw new Error("Uh oh, no prompt was provided");
+//     }
+
+//     const response = await openai.createCompletion({
+//       model: "text-davinci-003",
+//       prompt,
+//       max_tokens: 64,
+//     });
+
+//     const completion = response.data.choices[0].text;
+
+//     return res.status(200).json({
+//       success: true,
+//       message: completion,
+//     });
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// });
+
+// make this request a commmand line req
+
+const askOpenAI = async () => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+
+  logRed("Please enter the prompt:");
+
+  const prompt = await new Promise((resolve) => {
+    rl.question("", (answer) => {
+      resolve(answer);
+      rl.close();
+    });
+  });
 
   try {
-    if (prompt == null) {
+    if (!prompt) {
       throw new Error("Uh oh, no prompt was provided");
     }
 
-    const response = await openai.createCompletion({
+    logGreen(`User prompt: ${prompt}`);
+
+    const response = await  openai.chat.completions.create({
       model: "text-davinci-003",
       prompt,
       max_tokens: 64,
@@ -30,13 +80,11 @@ app.post("/ask", async (req, res) => {
 
     const completion = response.data.choices[0].text;
 
-    return res.status(200).json({
-      success: true,
-      message: completion,
-    });
+    // Log the response in green color
+    logGreen(`Response: ${completion}`);
   } catch (error) {
-    console.log(error.message);
+    console.log(chalk.red(error.message));
   }
-});
+};
 
-app.listen(port, () => console.log(`Server is running on port ${port}!!`));
+askOpenAI();
